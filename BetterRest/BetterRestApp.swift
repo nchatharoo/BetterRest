4639aa574039c1a959fbfe75767eac11af57a733
Nadheer Chatharoo <nchatharoo@icloud.com>
2021-04-12 14:11:25 +0200

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Nadheer on 12/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
