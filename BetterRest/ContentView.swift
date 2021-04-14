aa62a402686797c923dc64cb42ee667530b6b17f
Nadheer Chatharoo <nchatharoo@icloud.com>
2021-04-14 11:53:13 +0200

    Replace alert with a Section displaying dynamically the recommended bedtime

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -19,16 +19,11 @@ struct ContentView: View {
     @State private var wakeUp = defaultWakeTime
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
-    
-    //MARK : Alert
-    @State private var alertTitle = ""
-    @State private var alertMessage = ""
-    @State private var showingAlert = false
-    
+        
     // MARK : Model
     let model = SleepCalculator()
     
-    func calculateBedTime() {
+    func calculateBedTime() -> String {
         let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
         let hour = (components.hour ?? 0) * 60 * 60
         let minute = (components.minute ?? 0) * 60
@@ -39,20 +34,23 @@ struct ContentView: View {
             
             let formatter = DateFormatter()
             formatter.timeStyle = .short
-
-            alertTitle = "Your ideal bedtime is…"
-            alertMessage = formatter.string(from: sleepTime)
+            return formatter.string(from: sleepTime)
             
         } catch {
-            alertTitle = "Error"
-            alertMessage = "Sorry, there was a problem calculating your bedtime."
+            return "Sorry, there was a problem calculating your bedtime."
         }
-        showingAlert = true
     }
     
     var body: some View {
         NavigationView {
             Form {
+                
+                Section(header: Text("Your recommended bed time is...")) {
+                    Text(calculateBedTime())
+                        .font(.largeTitle)
+                        .foregroundColor(Color(UIColor.systemGreen))
+                }
+                
                 VStack(alignment: .leading) {
                     Text("When do you want to wake up ?")
                         .font(.headline)
@@ -78,15 +76,7 @@ struct ContentView: View {
                     }
                 }
             }
-            .alert(isPresented: $showingAlert) {
-                Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
-            }
             .navigationBarTitle("Better Rest")
-            .navigationBarItems(trailing:
-                                    Button(action: calculateBedTime, label: {
-                                        Text("Calculate")
-                                    })
-            )
         }
     }
 }
